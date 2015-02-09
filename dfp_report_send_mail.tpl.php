a4db94b4597b068df9e7dd4a88857b7870fd5a09
tn <thuan@Thuans-MacBook-Pro.local>
2015-02-09 14:54:21 -0800

    reformart info sending email

--- dfp_report_send_mail.tpl.php
+++ dfp_report_send_mail.tpl.php
@@ -4,31 +4,42 @@
  * Theme implementation to display the Google Analytics summary.
  */
 ?>
+<?php
+switch ($date) {
+    case "daily":
+        $time = date('m/d/Y', strtotime('yesterday'));
+        $report='Report for day: '.$time;
+        break;
+    case "weekly":
+        $week_start = date('m/d/Y', strtotime('last week monday'));
+		$week_end = date('m/d/Y', strtotime('last week sunday'));
+		$report = 'Report From: '.$week_start.' To '.$week_end;
+        break;
+    case "monthly":
+        $first = date('m/d/Y', strtotime('first day of last month'));
+		$last = date('m/d/Y', strtotime('last day of last month'));
+		$report = 'Report From: '.$first.' To '.$last;
+        break;
+}
+?>
 <div style="width:100%;">
   <div>
-    <h3 style="text-transform: capitalize;"><?php print $date ; ?> Report:</h3>
-	<h3>This is a report for <?php print $summary[1][1]; ?> with AD id is: <?php print print $summary[1][0];?>:</h3>
+    <h3 style="text-transform: capitalize;"><?php print $report ; ?></h3>
+	<h3><?php print $title; ?></h3>
   </div>
 	<table style="width:100%">
     <tr style="background: #e4e4e4;">
-	  <th style ="width:20%;padding:2px 0;"><?php print t('LINE_ITEM_NAME'); ?></th>
-	  <td style ="width:20%;padding:2px 0;"><?php print $summary[1][1]; ?></td>
-      <th style ="width:20%;padding:2px 0;"><?php print t('AD_SERVER_IMPRESSIONS'); ?></th>
+      <th style ="width:30%;padding:2px 0;"><?php print t('IMPRESSIONS'); ?></th>
       <td style ="width:20%;padding:2px 0;"><?php print $summary[1][2]; ?></td>
 
     </tr>
     <tr style="background: #efefef;">
-	  <th style ="width:20%;padding:2px 0;"><?php print t('AD_SERVER_CLICKS'); ?></th>
+	  <th style ="width:20%;padding:2px 0;"><?php print t('CLICKS'); ?></th>
       <td style ="width:20%;padding:2px 0;"><?php print $summary[1][3]; ?></td>
-	  <th style ="width:20%;padding:2px 0;"><?php print t('AD_SERVER_CTR'); ?></th>
-      <td style ="width:20%;padding:2px 0;"><?php print $summary[1][4]; ?></td>
-     
     </tr>
     <tr style="background: #e4e4e4;">
-      <th style ="width:20%;padding:2px 0;"><?php print t('AD_SERVER_CPM_AND_CPC_REVENUE'); ?></th>
-	  <td style ="width:20%;padding:2px 0;"><?php print $summary[1][5]; ?></td>
-	  <th style ="width:20%;padding:2px 0;"><?php print t('AD_SERVER_WITHOUT_CPD_AVERAGE_ECPM'); ?></th>
-      <td style ="width:20%;padding:2px 0;"><?php print $summary[1][6]; ?></td>
+      <th style ="width:20%;padding:2px 0;"><?php print t('CTR (Click Through Ratio)'); ?></th>
+      <td style ="width:20%;padding:2px 0;"><?php print ((float)$summary[1][4])*100; ?>%</td>
     </tr>
   </table>
 </div>
